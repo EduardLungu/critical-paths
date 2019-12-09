b5d87f8da4b9bf93e0ffa4403b46f582fac0cb5d
EduardLungu <lungueduard94@gmail.com>
2019-12-09 22:52:05 +0200

    Finished purchase tests

--- emag/src/automationFramework/BuyProduct.java
+++ emag/src/automationFramework/BuyProduct.java
@@ -1,34 +1,135 @@
 package automationFramework;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BuyProduct {
 	public static void buyWithCreditCard(WebDriver driverToken) {
 		WebDriver driver = driverToken;
-	 
-		//Print a message that opening was successful
-		 System.out.println("Successfully opened the eMAG Homepage.");
+		
+		//Starting on homepage
+		//Clicking on the first product they show
+		driver.findElement(By.className("product-title")).click();
+		
+		//Clicking "Adauga in cos"
+		driver.findElement(By.xpath("//button[contains(text(),'Adauga in Cos')]")).click();
+		
+		//Clicking "Vezi detalii cos"
+		//driver.findElement(By.linkText("https://www.emag.ro/cart/products?ref=add-to-cart-module_go-to-cart_button"));
+		driver.get("https://www.emag.ro/cart/products?ref=add-to-cart-module_go-to-cart_button");
+		
+		//Go to Checkout
+		driver.get("https://www.emag.ro/cart/checkout");
+		
+		//Login
+		//Input email
+		driver.findElement(By.id("email")).sendKeys("m.dante.games@gmail.com");
 		 
+		//Click continue
+		driver.findElement(By.className("auth-submit")).click();
+		 
+		//Input password
+		driver.findElement(By.id("password")).sendKeys("testare automata 1");
+		
+		//Click continue
+		driver.findElement(By.className("auth-submit")).click();
+		
+		//Select "Livrare prin curier"
+		driver.findElement(By.id("courierTab")).click();
+		
+		/* 
+		-This section is relevant only if the user didn't set an address yet 
+		-For ulterior testing, this section is skipped
+		//Click "adauga" in the section "Adresa de livrare"
+		driver.findElement(By.className("address-add-new")).click();
+		
+		//Add name
+		driver.findElement(By.name("name")).sendKeys("Flemu");
+		
+		//Add phone
+		driver.findElement(By.id("phone")).sendKeys("0777777777");
+		
+		//Add address
+		driver.findElement(By.name("street_address")).sendKeys("Calea Frumoasa nr. 1, sc. A, et. 4, ap. 94");
+		
+		//Click "Salveaza"
+		driver.findElement(By.xpath("//button[contains(text(),'Salveaza')]")).click();
+		*/
+		
+		//Choose payment method card
+		driver.findElement(By.id("paymentLinecard")).click();
+		
+		/*Click "Pasul urmator"
+		-This test should be done only on testing environments
+		-Since I'm testing on the production website, I will comment this section
+		driver.get("https://www.emag.ro/cart/summary");
+		*/
 
-		 
-		 //Close the window
-		 driver.quit();
-		 System.out.println("Successfully closed the eMAG Homepage.");
 	}
 	
 	public static void buyWithCash(WebDriver driverToken) {
 		WebDriver driver = driverToken;
-	 
-		//Print a message that opening was successful
-		 System.out.println("Successfully opened the eMAG Homepage.");
+		
+		//Starting on homepage
+		//Clicking on the first product they show
+		driver.findElement(By.className("product-title")).click();
+		
+		//Clicking "Adauga in cos"
+		driver.findElement(By.xpath("//button[contains(text(),'Adauga in Cos')]")).click();
+		
+		//Clicking "Vezi detalii cos"
+		//driver.findElement(By.linkText("https://www.emag.ro/cart/products?ref=add-to-cart-module_go-to-cart_button"));
+		driver.get("https://www.emag.ro/cart/products?ref=add-to-cart-module_go-to-cart_button");
+		
+		//Go to Checkout
+		driver.get("https://www.emag.ro/cart/checkout");
+		
+		//Login
+		//Input email
+		driver.findElement(By.id("email")).sendKeys("m.dante.games@gmail.com");
 		 
-
-		 
-		 //Close the window
-		 driver.quit();
-		 System.out.println("Successfully closed the eMAG Homepage.");
+		//Click continue
+		driver.findElement(By.className("auth-submit")).click();
+		
+		//Input password
+		driver.findElement(By.id("password")).sendKeys("testare automata 1");
+		
+		//Click continue
+		driver.findElement(By.className("auth-submit")).click();
+		
+		//Select "Livrare prin curier"
+		driver.findElement(By.id("courierTab")).click();
+		
+		/* 
+		-This section is relevant only if the user didn't set an address yet 
+		//Click "adauga" in the section "Adresa de livrare"
+		driver.findElement(By.className("address-add-new")).click();
+		
+		//Add name
+		driver.findElement(By.name("name")).sendKeys("Flemu");
+		
+		//Add phone
+		driver.findElement(By.id("phone")).sendKeys("0777777777");
+		
+		//Add address
+		driver.findElement(By.name("street_address")).sendKeys("Calea Frumoasa nr. 1, sc. A, et. 4, ap. 94");
+		
+		//Click "Salveaza"
+		driver.findElement(By.xpath("//button[contains(text(),'Salveaza')]")).click();
+		*/
+		
+		//Choose payment method cash (ramburs la curier)
+		driver.findElement(By.className("payment-method-numerar")).click();
+		
+		/*Click "Pasul urmator"
+		-This test should be done only on testing environments
+		-Since I'm testing on the production website, I will comment this section
+		driver.get("https://www.emag.ro/cart/summary");
+		*/
 	}
 	 
 }
--- emag/src/automationFramework/MainTests.java
+++ emag/src/automationFramework/MainTests.java
@@ -7,27 +7,34 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class MainTests {
 
 	public static void main(String[] args) throws InterruptedException {
-		 //Create a new instance of ChromeDriver
-		 WebDriver driver = new ChromeDriver();
-		 
-		 //Test the emag homepage
-		 driver.get("http://www.emag.ro/homepage");
-		 Homepage.checkHomepage(driver);
+		 		
+		 //Test the homepage
+		 WebDriver driverHomepage = new ChromeDriver();
+		 driverHomepage.get("http://www.emag.ro/homepage");
+		 Homepage.checkHomepage(driverHomepage);
+		 driverHomepage.close();
+		 System.out.println("The homepage tests have finished running.");
 		 
 		 //Test the user login
-		 driver.get("http://www.emag.ro/user/login");
-		 Login.userLogin(driver);
+		 WebDriver driverLogin = new ChromeDriver();
+		 driverLogin.get("http://www.emag.ro/user/login");
+		 Login.userLogin(driverLogin);
+		 driverLogin.close();
+		 System.out.println("The Login tests have finished running.");
 		 
-		 //Test product purchase. The test starts with the login form, so the driver doesn't need to be updated
-		 //BuyProduct.buyWithCreditCard(driver);
-		 //BuyProduct.buyWithCash(driver);
+		 //Test product purchase with a credit card
+		 WebDriver driverPurchaseCard = new ChromeDriver();
+		 driverPurchaseCard.get("http://www.emag.ro/homepage");
+		 BuyProduct.buyWithCreditCard(driverPurchaseCard);
+		 driverPurchaseCard.close();
+		 System.out.println("The cash purchase tests have finished running.");
 		 
-		 //The tests have finished running
-		 System.out.println("All tests have finished running.");
-		 
-		 //Close the window
-		 driver.close();
-		 System.out.println("Successfully closed the window.");
+		 //Test product purchase with cash
+		 WebDriver driverPurchaseCash = new ChromeDriver();
+		 driverPurchaseCash.get("http://www.emag.ro/homepage");
+		 BuyProduct.buyWithCash(driverPurchaseCash);
+		 driverPurchaseCash.close();
+		 System.out.println("The card purchase tests have finished running.");
 	}
 
 }
